fc090680d9ba802e632abab8c8382d0e2a78ad65
mvanarnhem <150653486+mvanarnhem@users.noreply.github.com>
2025-01-27 15:44:21 +0100

    titels veranderd grafieken links + index_test.html meer geordend

--- code/radial_plot.js
+++ code/radial_plot.js
@@ -294,6 +294,16 @@ let global_data = []
 const sortedYearRanges = window.selectedYearRanges.sort((a, b) => a[0] - b[0]);
 
 function update_radial_features(filtered_data_input) {
+    const header_linegraph = d3.select("#heading-container-radial-year");
+    if (window.selectedRange.length == 0){
+        header_linegraph.html(`Weekly Scores All Features Averaged over <br> 1965 - 2023`);
+    }
+    else if (window.selectedRange[0] == window.selectedRange[1]){
+        header_linegraph.html(`Weekly Scores All Features Averaged over <br> ${window.selectedRange[0]}`);
+    }
+    else if (window.selectedRange.length == 2){
+        header_linegraph.html(`Weekly Scores All Features Averaged over <br> ${window.selectedRange[0]} - ${window.selectedRange[1]}`);
+    }
     global_data = filtered_data_input;
     const selectedYearRanges = window.selectedYearRanges.sort((a, b) => a[0] - b[0]);
     const currentYearRange = selectedYearRanges[currentYearRangeIndex];
@@ -307,6 +317,16 @@ function update_radial_features(filtered_data_input) {
 }
 
 window.addEventListener("selectedRangeUpdated", function () {
+    const header_linegraph = d3.select("#heading-container-radial-year");
+    if (window.selectedRange.length == 0){
+        header_linegraph.html(`Weekly Scores All Features Averaged over <br> 1965 - 2023`);
+    }
+    else if (window.selectedRange[0] == window.selectedRange[1]){
+        header_linegraph.html(`Weekly Scores All Features Averaged over <br> ${window.selectedRange[0]}`);
+    }
+    else if (window.selectedRange.length == 2){
+        header_linegraph.html(`Weekly Scores All Features Averaged over <br> ${window.selectedRange[0]} - ${window.selectedRange[1]}`);
+    }
     const selectedRange = window.selectedRange; 
     const selectedYearRanges = window.selectedYearRanges.sort((a, b) => a[0] - b[0]);
     const currentYearRange = selectedRange;
